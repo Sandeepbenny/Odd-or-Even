51839fcd1bd788c4657181ab05b66b173c9a7d0c
Sandeepbenny <sandeep@sanduchackonal@gmail.com>
2022-04-24 22:36:29 +0530

    Code executed

---
+++ C program to check if a number is odd or even.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    if(num % 2 == 0)
+        printf("%d is even.", num);
+    else
+        printf("%d is odd.", num);    
+}
